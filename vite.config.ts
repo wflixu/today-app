4e11942ca8441b9db66081a09d4ef0f16500e755
wflixu <wflixu@88.com>
2021-05-03 23:23:28 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    cors: true,
+    proxy: {
+      '^/api/.*': {
+        target: 'http://127.0.0.1:7001',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/fallback/, '')
+      }
+    }
+  }
+})
